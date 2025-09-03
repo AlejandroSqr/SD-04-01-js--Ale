1dd90113130ee43fd92d693c4034bceab1238572
AlejandroDel <alejandrohdzd@proton.me>
2025-09-03 16:28:21 -0600

    Update task1.js
    Resuelto

--- src/task1.js
+++ src/task1.js
@@ -2,7 +2,8 @@ const arr = []
 
 // Escribe tu codigo por debajo de esta linea
 
-
+const arr = []
+arr.push("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20");
 
 // Escribe tu codigo por encima de esta linea
 
