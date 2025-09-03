2ec9bf94d8c6880b19e3d2495a35ab4eb7f4f82f
AlejandroDel <alejandrohdzd@proton.me>
2025-09-03 16:37:13 -0600

    Update task4.js
    Resuelto

--- src/task4.js
+++ src/task4.js
@@ -1,10 +1,8 @@
 const arr = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14]
 
 // Escribe tu codigo por debajo de esta linea
-
-
-
-// TEscribe tu codigo por encima de esta linea
+arr.splice(5, 1)
+// Escribe tu codigo por encima de esta linea
 
 //No tocar esta funcion
 export function taskArr(){
